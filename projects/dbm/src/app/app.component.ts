1fef92a124c393373d34641aa9c71a3de5ea3176
Miroslav Bozik <mbozik@pdinet.com>
2021-08-08 14:17:00 +0200

    installed dexie

--- projects/dbm/src/app/app.component.ts
+++ projects/dbm/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import Dexie from 'dexie';
 
 @Component({
   selector: 'app-idxdbm',
@@ -6,6 +7,34 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    Dexie.getDatabaseNames()
+    .then(results => {
+      this.databases = results;
+
+       const firstDb = new Dexie(this.databases[0]);
+       firstDb.open().then(()=>{
+
+        firstDb.tables.forEach(t => {
+          console.log(`table ${t.name}`);
+        });
+
+       });
+    });
+
+
+    // const db = new Dexie('test'+new Date().getTime());
+    // db.version(1).stores({
+    //   test: 'id'
+    // });
+    //db.open();
+  }
+
   title = 'dbm';
+
+  databases: string[] = [];
+
+
 }
